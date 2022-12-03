e88cc63e1a27d9a197c3160f91f2430f148fa04b
leonidSpiri <2003.leonid2003@gmail.com>
2022-12-03 21:03:00 +0300

    cool shit

--- api_requests/request.py
+++ api_requests/request.py
@@ -1,10 +1,10 @@
-import config
-
 import requests
+
+from ..settings import api_config
 
 
 def get_place_coord(city):
-    payload = {'geocode': city, 'apikey': config.geo_key, 'format': 'json'}
+    payload = {'geocode': city, 'apikey': api_config.geo_key, 'format': 'json'}
     r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
     geo = r.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
     return geo
@@ -13,9 +13,6 @@ def get_place_coord(city):
 def get_weather(city):
     coordinates = get_place_coord(city).split()
     payload = {'lat': coordinates[1], 'lon': coordinates[0], 'lang': 'ru_RU'}
-    r = requests.get('https://api.weather.yandex.ru/v2/informers', params=payload, headers=config.weather_key)
+    r = requests.get('https://api.weather.yandex.ru/v2/informers', params=payload, headers=api_config.weather_key)
     weather_data = r.json()
     return print(weather_data)
-
-
-get_weather('Ленинградская область симагино снт надежда')
---
+++ database/orm.py
@@ -0,0 +1,3 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
